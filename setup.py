c623e3f7880a956e8238037d65b4b4125907402e
hoto17296 <mail@hoto.me>
2017-04-26 12:17:57 +0900

    fix requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
 from setuptools import setup
 
+install_requires = [
+    'flask',
+]
+
+setup_requires=[
+    'pytest-runner',
+]
+
+tests_require = [
+    'pytest',
+]
+
+dev_require = [
+]
+
+doc_require = [
+    'sphinx',
+]
+
 setup(
     name='minitwit',
     packages=['minitwit'],
     include_package_data=True,
-    install_requires=[
-        'flask',
-    ],
-    setup_requires=[
-        'pytest-runner',
-    ],
-    tests_require=[
-        'pytest',
-    ],
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    extras_require = {
+        'dev': dev_require + tests_require + doc_require,
+        'test': tests_require,
+        'doc': doc_require,
+    },
 )
